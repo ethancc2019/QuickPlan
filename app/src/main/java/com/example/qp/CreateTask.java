311776eee86c25f22e2e655cb73c52586711d91c
ethancc2019 <ethan.collins335@myci.csuci.edu>
2019-03-04 14:53:26 -0800

    Commit on this branch

--- app/src/main/java/com/example/qp/CreateTask.java
+++ app/src/main/java/com/example/qp/CreateTask.java
@@ -1,11 +1,10 @@
 package com.example.qp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.*;
 import android.os.Bundle;
 
 public class CreateTask extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
