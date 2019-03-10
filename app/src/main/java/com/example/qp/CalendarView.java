82b2751356f61ed46a2e6805e925c4de1047ea87
John Storie <johnhstorie@gmail.com>
2019-03-09 23:22:49 -0700

    working on recyclerview for calendar

--- app/src/main/java/com/example/qp/CalendarView.java
+++ app/src/main/java/com/example/qp/CalendarView.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.support.annotation.NonNull;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class CalendarView extends AppCompatActivity {
+
+    ArrayList<String>taskNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +24,16 @@ public class CalendarView extends AppCompatActivity {
         setContentView(R.layout.activity_calendar_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        android.widget.CalendarView calendar = (android.widget.CalendarView) findViewById(R.id.calendarView);
+        android.widget.CalendarView calendar = findViewById(R.id.calendarView);
+        nameTester();
+
         calendar.setOnDateChangeListener(new android.widget.CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull android.widget.CalendarView view, int year, int month, int dayOfMonth) {
 
             }
         });
+
 
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +43,24 @@ public class CalendarView extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+    }
+
+    private void nameTester(){
+        taskNames.add("Task 1");
+        taskNames.add("Task 2");
+        taskNames.add("Task 3");
+        taskNames.add("Task 4");
+        taskNames.add("Task 5");
+        taskNames.add("Task 6");
+        taskNames.add("Task 7");
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.task_recycler);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, taskNames);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
